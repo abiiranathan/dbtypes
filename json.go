7c758f73334bba7f90a1d65ee74ce02f810b19fa
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-06-25 12:42:10 +0300

    fix: add GormDataType and gob encoding support

--- json.go
+++ json.go
@@ -1,14 +1,23 @@
 package dbtypes
 
 import (
+	"bytes"
 	"database/sql/driver"
+	"encoding/gob"
 	"encoding/json"
+	"fmt"
 )
 
-// JSON implements the database/sql/driver Scanner and Valuer interfaces.
+// JSON implements the database/sql/driver Scanner and Valuer interfaces,
+// as well as gob.GobEncoder and gob.GobDecoder interfaces.
 type JSON map[string]interface{}
 
-// Scan scan value into Jsonb, implements sql.Scanner interface
+func init() {
+	// Register JSON type for gob encoding/decoding
+	gob.Register(JSON{})
+}
+
+// Scan scans a value into JSON, implements sql.Scanner interface
 func (j *JSON) Scan(value interface{}) error {
 	if err := json.Unmarshal(value.([]byte), &j); err != nil {
 		return err
@@ -16,8 +25,35 @@ func (j *JSON) Scan(value interface{}) error {
 	return nil
 }
 
-// Value return json value, implement driver.Valuer interface
+// Value returns the JSON value, implements driver.Valuer interface
 func (j JSON) Value() (driver.Value, error) {
 	valueString, err := json.Marshal(j)
 	return string(valueString), err
+}
+
+// Custom function used by the gorm ORM if used.
+func (j JSON) GormDataType() string {
+	return "jsonb"
+}
+
+// GobEncode encodes the JSON value using gob encoding.
+func (j JSON) GobEncode() ([]byte, error) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(j)
+	if err != nil {
+		return nil, fmt.Errorf("error encoding JSON: %v", err)
+	}
+	return buffer.Bytes(), nil
+}
+
+// GobDecode decodes the gob-encoded data into a JSON value.
+func (j *JSON) GobDecode(data []byte) error {
+	buffer := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(buffer)
+	err := decoder.Decode(&j)
+	if err != nil {
+		return fmt.Errorf("error decoding JSON: %v", err)
+	}
+	return nil
 }
