fa4ec78f3af37737e88392761a99d368110f90c5
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-09-11 15:20:15 +0300

    Update date.go
    Add ParseDateFromString function

--- date.go
+++ date.go
@@ -107,3 +107,19 @@ func (date Date) String() string {
 func NewDate(year int, month time.Month, day int) Date {
 	return Date(time.Date(year, month, day, 0, 0, 0, 0, time.Local))
 }
+
+func ParseDateFromString(dateStr string) (Date, error) {
+	var date Date
+
+	b, err := json.Marshal(dateStr)
+	if err != nil {
+		return date, err
+	}
+
+	err = date.UnmarshalJSON(b)
+	if err != nil {
+		return date, err
+	}
+	return date, nil
+}
+
